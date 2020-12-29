6a5c16807cdcc48c57b1f839754fe842a2f64023
Tristan Joshua Alba <tristan.j.alba@gmail.com>
2020-12-29 22:11:59 +0800

    Add database connection

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-      host : process.env.DATABASE_URL,
-      ssl: true,
+        connectionString: process.env.DATABASE_URL,
+        ssl: true,
     //   ssl: {
     //     rejectUnauthorized: false
     //   }
