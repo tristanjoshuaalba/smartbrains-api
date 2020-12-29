715cd930d1e1b8091a3d9e04b2672184e61fa007
Tristan Joshua Alba <tristan.j.alba@gmail.com>
2020-12-29 22:06:42 +0800

    Add database connection: syntax fix

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-      host : rocess.env.DATABASE_URL,
+      host : process.env.DATABASE_URL,
       ssl: true,
     //   ssl: {
     //     rejectUnauthorized: false
