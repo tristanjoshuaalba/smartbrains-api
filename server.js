0c666f51d83ec38b630809dd5f1b9b9c881dc1bb
Tristan Joshua Alba <tristan.j.alba@gmail.com>
2020-12-29 22:15:51 +0800

    Add database connection: Node.js fix

--- server.js
+++ server.js
@@ -14,10 +14,9 @@ const db = knex({
     client: 'pg',
     connection: {
         connectionString: process.env.DATABASE_URL,
-        ssl: true,
-    //   ssl: {
-    //     rejectUnauthorized: false
-    //   }
+        ssl: {
+            rejectUnauthorized: false
+          }
     }
   });
 
